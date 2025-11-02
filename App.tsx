39237391d0798f8951948550e1f35c7c0b7fe25c
Frederic Medery <fmedery@google.com>
2025-11-02 08:32:41 -0500

    try to fix gtag

--- App.tsx
+++ App.tsx
@@ -95,26 +95,6 @@ function App() {
     gtag.pageview(new URL(window.location.href));
   }, [step]);
 
-  useEffect(() => {
-    console.log('GA: Initializing GA script');
-    const script = document.createElement('script');
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`;
-    script.async = true;
-    script.onload = () => {
-      console.log('GA: gtag.js script loaded');
-    };
-    document.head.appendChild(script);
-
-    const script2 = document.createElement('script');
-    script2.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${gtag.GA_TRACKING_ID}');
-      console.log('GA: GA configured');
-    `;
-    document.head.appendChild(script2);
-  }, []);
 
   const renderContent = () => {
     if (error) {
